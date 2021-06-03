872c422771a2f01b14e1cd456d33097fa6177e64
MiGoYAm <miroslawjakiela@gmail.com>
2021-06-03 11:46:31 +0200

    initial

---
+++ src/main/java/me/migoyam/zamknijsie/ZamknijSie.java
@@ -0,0 +1,39 @@
+package me.migoyam.zamknijsie;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+public final class ZamknijSie extends JavaPlugin implements Listener {
+    private BukkitTask task;
+    private final short minutes = (short) getConfig().getInt("serverShutdownAfter");
+    private final String string (){
+        if(minutes == 1) {
+            return " minute.";
+        }else
+            return " minutes.";
+    }
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        timer();
+    }
+    public void timer(){
+        if(Bukkit.getOnlinePlayers().size() <= 1) {
+            getLogger().info("No players online. Server shutdowns in " + minutes + string());
+            task = Bukkit.getScheduler().runTaskLater(this,()->getServer().shutdown(),minutes * 1200L);}}
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Bukkit.getScheduler().cancelTask(task.getTaskId());
+    }
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event){
+        timer();
+    }
+}
+// if(task.isCanceled)
